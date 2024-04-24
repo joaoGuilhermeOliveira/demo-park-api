a5768ade138bdd747882c755cbbcf84ddb93204a
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-04-24 11:04:37 -0300

    first commit

---
+++ src/main/java/com/mbalem/demoparkapi/config/SprintTimezoneConfig.java
@@ -0,0 +1,15 @@
+package com.mbalem.demoparkapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SprintTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
