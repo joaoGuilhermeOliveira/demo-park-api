36464ce62a814fa726a7f6cac3c70e021f912139
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-05-03 10:18:25 -0300

    Adicionando DTO no recurso de alteração de senha

--- src/main/java/com/mbalem/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/mbalem/demoparkapi/web/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.mbalem.demoparkapi.entities.Usuario;
 import com.mbalem.demoparkapi.service.UsuarioService;
 import com.mbalem.demoparkapi.web.controller.dto.UsuarioCreateDto;
 import com.mbalem.demoparkapi.web.controller.dto.UsuarioResponseDto;
+import com.mbalem.demoparkapi.web.controller.dto.UsuarioSenhaDto;
 import com.mbalem.demoparkapi.web.controller.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -32,9 +33,9 @@ public class UsuarioController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
-        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
-        return ResponseEntity.ok(user);
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto) {
+        Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
